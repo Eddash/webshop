6e6773decd6bfe36c3933bfc546e161f5d3ad345
Eddash <seibel.edgar@gmail.com>
2016-01-08 16:15:48 +0100

    mit daten gefuellt db seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :whiskies
+  resources :whiskies, only: [:show, :index]
+
+  root 'whiskies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
