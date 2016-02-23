3353026c359b509fe54126a3c644155f7efba801
Eddash <seibel.edgar@gmail.com>
2016-02-23 14:12:29 +0100

    bilder werden nun direkt aus assets ordner gezogen

--- app/models/whisky.rb
+++ app/models/whisky.rb
@@ -2,12 +2,12 @@ class Whisky < ActiveRecord::Base
   ratyrate_rateable 'geschmack', 'geruch', 'flaschendesign', 'preisleistung'
 
     def pic
-      "http://www2.pic-upload.de/img/#{pic_url}"
+      "#{pic_url}"
     end
 
     def cart_action(current_user_id)
       if $redis.sismember "cart#{current_user_id}", id
-        "Entfrenen aus"
+        "Entfernen aus"
       else
         "Hinzufügen zum"
       end
