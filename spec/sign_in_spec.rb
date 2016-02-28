8e285c80bb732932feabad60da00bb3f1ecdb229
kaethles <akaethner@outlook.de>
2016-02-28 22:50:12 +0100

    Test bugfix

--- spec/sign_in_spec.rb
+++ spec/sign_in_spec.rb
@@ -1,33 +1,39 @@
-require 'rails_helper'  
+require 'rails_helper'
 
 RSpec.feature 'Anmelden',
-              'Als ein registrierter "User", 
-               will Ich mich einlogen können 
-               um meine Einkäufe verwalten zu können' do
-  context 'Auf Startseite' do 
-    let(:user) { FactoryGirl.create(:user) } 
+              'Als registrierter "User",
+               will ich mich einloggen und
+               meinen Einkäufe verwalten können' do
 
-    scenario 'Anmelden' do 
-      visit root_path  
+  context 'Auf Startseite' do
+    let(:user) {FactoryGirl.create(:user)}
 
-      click_link 'Anmelden' 
-      fill_in 'E-Mail', with: user.email 
-      fill_in 'Passwort', with: 'Kappa123'   
+    scenario 'Anmelden' do
+      visit root_path
 
-      click_button 'Anmelden' 
-      expect(page).to have_content "#{user.email}" 
-    end  
+      click_link 'Anmelden'
+      fill_in 'Email', with: user.email
+      fill_in 'Passwort', with: 'Kappa123'
 
-    scenario 'Abmelden' do 
-      visit root_path  
+      click_button 'Anmelden'
 
-      click_link 'Anmelden' 
-      fill_in 'E-Mail', with: user.email 
-      fill_in 'Passwort', with: 'Kappa123'  
+      expect(page).to have_content "#{user.email}"
+    end
 
-      expect(page).to_not have_content "#{user.email}" 
-      expect(page).to have_content 'Anmelden' 
-    end 
-  end 
+    scenario 'Abmelden' do
+      visit root_path
+
+      click_link 'Anmelden'
+      fill_in 'Email', with: user.email
+      fill_in 'Passwort', with: 'Kappa123'
+
+      click_button 'Anmelden'
+      click_link 'Abmelden'
+
+      expect(page).to_not have_content "#{user.email}"
+      expect(page).to have_content 'Anmelden'
+    end
+
+  end
 end
 
